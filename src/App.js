2236fa8a73aabad9f070585ebe1a416015c88529
williansamer <willian.samer@gmail.com>
2021-11-29 16:51:43 -0300

    list partially concluded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Tmdb from "./Tmdb";
+
+import MovieRow from './components/MovieRow'
 
 function App() {
+  const [moveList, setMoveList] = useState([]);
+
+  useEffect(() => {
+    const loadList = async () => {
+      const list = await Tmdb.getHomeList();
+      console.log(list);
+      setMoveList(list);
+    };
+
+    loadList();
+  }, []);
+
   return (
     <div className="App">
-      Hello World!!!
+      <section className="lists">
+        {moveList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
